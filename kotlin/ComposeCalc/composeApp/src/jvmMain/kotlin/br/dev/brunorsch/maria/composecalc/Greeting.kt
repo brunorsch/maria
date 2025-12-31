66cdb4b9d8fdabf27fe1bb4674cf3fff67748d24
Bruno Ribeiro Schmidt <brunoribeiro.sch@gmail.com>
2025-12-31 00:07:52 -0300

    remove classes geradas pelo gerador do KMP

--- kotlin/ComposeCalc/composeApp/src/jvmMain/kotlin/br/dev/brunorsch/maria/composecalc/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package br.dev.brunorsch.maria.composecalc
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
